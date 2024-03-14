2485d4e0495f9f93dc9c285efdb797e18e40f6d3
SoltidueMarquez <2630388173@qq.com>
2024-03-14 21:52:17 +0800

    圣物UI动效实现3/3

---
+++ Assets/Scripts/UI/UIObjectEffects.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum  State
+{
+    PointerChosen,
+    None
+}
+
+namespace UI
+{
+    public class UIObjectEffects : MonoBehaviour
+    {
+        //TODO:之后要和逻辑上的价格合并
+        [Header("物品信息")]
+        [SerializeField, Tooltip("id")] protected string id;
+        [SerializeField,Tooltip("出售价格")]protected float salePrice;
+        [Multiline(4)]
+        [SerializeField, Tooltip("描述")] protected string desc;
+
+        [Header("UI组件")]
+        [SerializeField,Tooltip("说明UI")]protected GameObject descriptionCanvas;
+        [SerializeField,Tooltip("说明Text")]protected Text descriptionText;
+        [SerializeField, Tooltip("出售按钮")] protected GameObject saleButton;
+        [SerializeField,Tooltip("出售按钮Text")]protected Text saleButtonText;
+
+        protected State _state;
+        public Column _currentColumn;
+        
+        /// <summary>
+        /// 初始化函数
+        /// </summary>
+        public void Init()
+        {
+            descriptionText.text = desc;
+            saleButtonText.text = $"出售\n￥{salePrice}";
+            _state = State.None;
+            _currentColumn = UIManager.Instance.DetectColumn(gameObject, UIManager.Instance.sacredObjectColumns); //检测当前所在的物品栏
+            _currentColumn.bagObject = gameObject;//初始化当前所在的物品栏
+        }
+    }
+}
