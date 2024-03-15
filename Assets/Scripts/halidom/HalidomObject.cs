d578930e0d86bf3087fc71683862363bd13c0339
exilecai <1394661877@qq.com>
2024-03-15 13:01:58 +0800

    3.15

---
+++ Assets/Scripts/halidom/HalidomObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalidomObject 
+{
+    public string id;
+
+    public string halidomName;
+
+    public string description;
+
+    public BuffInfo buffInfo;
+
+    public HalidomObject()
+    {
+
+    }
+
+   
+}
