993783f302568b7411f916eb8840741922362631
SoltidueMarquez <2630388173@qq.com>
2024-03-16 18:57:12 +0800

    背包区骰面UI动效实现80%

--- Assets/Scripts/UI/UIObjectEffects.cs
+++ Assets/Scripts/UI/UIObjectEffects.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using UnityEngine.EventSystems;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public enum  State
 {
@@ -26,25 +25,26 @@ namespace UI
         [Header("UI组件")]
         [SerializeField,Tooltip("说明UI")]protected GameObject descriptionCanvas;
         [SerializeField,Tooltip("说明Text")]protected Text descriptionText;
-        [SerializeField,Tooltip("出售按钮")] protected GameObject saleButton;
+        [SerializeField,Tooltip("出售UI")] protected GameObject saleUI;
         [SerializeField,Tooltip("出售按钮Text")]protected Text saleButtonText;
 
         protected State _state;
-        protected Transform oldParent; //原本的父物体
+        protected Transform oldParent;  //原本的父物体
         public Column _currentColumn;   //当前所在栏位
-        
+
         /// <summary>
         /// 初始化函数
         /// </summary>
-        public void Init()
+        public void Init(List<Column> columns, float offset)
         {
             descriptionText.text = desc;
             saleButtonText.text = $"出售\n￥{salePrice}";
             _state = State.None;
-            _currentColumn = UIManager.Instance.DetectColumn(gameObject, UIManager.Instance.sacredObjectColumns); //检测当前所在的物品栏
-            _currentColumn.bagObject = gameObject;//初始化当前所在的物品栏
+            _currentColumn = UIManager.Instance.DetectColumn(gameObject, columns, offset); //检测当前所在的物品栏
+            if (_currentColumn != null) //初始化当前所在的物品栏
+            {
+                _currentColumn.bagObject = gameObject;
+            }
         }
-        
-        
     }
 }
