55aa4d5df2d8e32ebc2a4b232e19990bf277e8ca
SoltidueMarquez <2630388173@qq.com>
2024-03-19 18:22:01 +0800

    投掷结果UI生成封装

--- Assets/Scripts/UI/RollingResultDiceUI.cs
+++ Assets/Scripts/UI/RollingResultDiceUI.cs
@@ -1,36 +1,60 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace UI
 {
-    public class RollingResultDiceUI : MonoBehaviour
+    public class RollingResultDiceUI : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
     {
         [SerializeField,Tooltip("投掷结果序列号")]private int index;
         [SerializeField,Tooltip("所在战斗骰面位置")]private Vector2Int pageAndIndex;
+        [SerializeField, Tooltip("说明UI")] private GameObject infoCanvas;
+        [SerializeField, Tooltip("说明Text")] private Text infoText;
 
         /// <summary>
         /// 初始化函数
         /// </summary>
-        /// <param name="tmpIndex">序列号</param>
+        /// <param name="tmpIndex">结果序列号</param>
         /// <param name="position">坐标位置，先页面位置再页内位置</param>
-        public void Init(int tmpIndex, Vector2Int position)
+        /// <param name="id">骰面对应id</param>
+        public void Init(int tmpIndex, Vector2Int position, string id)
         {
             index = tmpIndex;
             pageAndIndex = position;
-        }
-        private void Start()
-        {
             this.GetComponent<Button>().onClick.AddListener(() =>
             {
                 BattleManager.Instance.parameter.playerChaStates.UseDice(index, BattleManager.Instance.currentSelectEnemy);
+                Destroy(gameObject);
             });
+            
+            //ToDo:根据id初始化信息
+            infoText.text = id;
         }
 
         private void JumpToPosition(Vector2Int position)
         {
             FightDicePageManager.Instance.SwitchToPosition(position);
         }
-        
+
+        private void RevertMark()
+        {
+            FightDicePageManager.Instance.RevertMarkColumn();
+        }
+
         //ToDo:写骰面预览效果
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            UIManager.Instance.EnterPreview(this.gameObject,UIManager.Instance.previewSizeS);
+            UIManager.Instance.DoShake(this.GetComponent<Image>(),UIManager.Instance.shakeAngleS);
+            JumpToPosition(pageAndIndex);
+            infoCanvas.SetActive(true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            UIManager.Instance.ExitPreview(gameObject);
+            RevertMark();
+            infoCanvas.SetActive(false);
+        }
     }
 }
