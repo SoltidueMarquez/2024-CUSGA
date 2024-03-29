4e69e85b81619ddb4ebba993acd24b5e816a9190
SoltidueMarquez <2630388173@qq.com>
2024-03-29 20:14:34 +0800

    更新重投次数文本

--- Assets/Scripts/UI/DataUIManager.cs
+++ Assets/Scripts/UI/DataUIManager.cs
@@ -6,9 +6,15 @@ namespace UI
     public class DataUIManager : MonoSingleton<DataUIManager>
     {
         [SerializeField, Tooltip("回合数文本")] private Text RunText;
+        [SerializeField, Tooltip("剩余重投次数")] private Text ReRollText;
         public void UpdateRunTimeText(int run)
         {
             RunText.text = $"{run}";
+        }
+        
+        public void UpdateReRollText(int time)
+        {
+            ReRollText.text = $"{time}";
         }
     }
 }
