76af9cbc4368b62f67787f89ed048d118a8b5eae
2333qbyqby <18317035095@163.com>
2024-04-08 19:24:59 +0800

    添加进入奖励界面添加金钱

---
+++ Assets/Scripts/SO/PlayerDataSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 在游戏过程中，玩家的数据会发生变化，这个类用于存储玩家的数据，是playerData上层的数据
+/// </summary>
+public class PlayerDataSO : ScriptableObject
+{
+   
+}
