987628e1aa1c04be3a773443d75a8ba99bc11a89
SoltidueMarquez <2630388173@qq.com>
2024-04-22 16:30:11 +0800

    金钱UI刷新函数更新

--- Assets/Scripts/UI/DataUIManager.cs
+++ Assets/Scripts/UI/DataUIManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using DG.Tweening;
 using UI.Store;
 using UnityEngine;
@@ -7,9 +8,12 @@ namespace UI
 {
     public class DataUIManager : MonoSingleton<DataUIManager>
     {
+        [SerializeField, Tooltip("动画时长")] private float animTime;
         [SerializeField, Tooltip("回合数文本")] private Text runText;
         [SerializeField, Tooltip("剩余重投次数")] private Text reRollText;
         [SerializeField, Tooltip("金钱文本")] private Text moneyText;
+        private int _currentMoney;//记录当前金币文本
+        
         public void UpdateRunTimeText(int run)
         {
             runText.text = $"{run}";
@@ -22,10 +26,39 @@ namespace UI
 
         public void UpdateMoneyText(int money)
         {
-            moneyText.DOText($"￥00000", 1f).OnComplete(() =>
+            var offset = _currentMoney - money;
+            if (offset == 0) { return; }
+
+            StartCoroutine(ChangeMoneyText(money));
+        }
+        IEnumerator ChangeMoneyText(int target)
+        {
+            var offset = Mathf.Abs(_currentMoney - target);
+            var step = offset switch
             {
-                moneyText.DOText($"￥{money}", 1f);
-            });
+                < 10 => 0.1f,
+                < 100 => 0.01f,
+                _ => 0.01f
+            };
+
+            while (true)
+            {
+                yield return new WaitForSeconds(step);
+                Debug.Log("每1秒执行一次");
+                if (_currentMoney < target)
+                {
+                    _currentMoney++;
+                }
+                else
+                {
+                    _currentMoney--;
+                }
+                moneyText.text = $"￥{_currentMoney}";
+                if (_currentMoney == target)
+                {
+                    break;
+                }
+            }
         }
     }
 }
