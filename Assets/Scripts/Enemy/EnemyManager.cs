cfb33584d5a7c3783774f928875a32fb24836f0e
2333qbyqby <18317035095@163.com>
2024-04-25 09:31:46 +0800

    修复小bug

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -10,27 +10,37 @@ public static class EnemyManager
     /// </summary>
     /// <param name="enemyType"></param>
     /// <returns></returns>
-    public static EnemyDataSO GetEnemyDataSOviaCondition(EnemyType enemyType,List<string> enemyIDs)
+    public static EnemyDataSO GetEnemyDataSOviaCondition(EnemyType enemyType, List<string> enemyIDs)
     {
         EnemyDataSO[] enemyDataSOs = Resources.LoadAll<EnemyDataSO>("Enemy");
         //这边先找出所有的敌人，然后再找出符合条件的敌人
         List<EnemyDataSO> resultList = new();
         for (int i = 0; i < enemyDataSOs.Length; i++)
         {
-            if (enemyDataSOs[i].enemyType == enemyType && !enemyIDs.Contains(enemyDataSOs[i].EnemyID))
+            if (enemyIDs != null)
             {
-                resultList.Add(enemyDataSOs[i]);
+                if (enemyDataSOs[i].enemyType == enemyType && !enemyIDs.Contains(enemyDataSOs[i].EnemyID))
+                {
+                    resultList.Add(enemyDataSOs[i]);
+                }
+            }
+            else
+            {
+                if (enemyDataSOs[i].enemyType == enemyType)
+                {
+                    resultList.Add(enemyDataSOs[i]);
+                }
             }
         }
         //var resultList = enemyDataSOs.Where(x => ((!enemyIDs.Contains(x.EnemyID)) && x.enemyType == enemyType)).ToList();
         Debug.Log("找到的敌人数量" + resultList.Count);
 
-        if(resultList.Count == 0)
+        if (resultList.Count == 0)
         {
             Debug.LogWarning("找不到符合要求的新敌人，将从旧的中随机挑选");
             resultList = enemyDataSOs.Where(x => x.enemyType == enemyType).ToList();
         }
-        
-        return resultList[Random.Range(0,resultList.Count)];
+
+        return resultList[Random.Range(0, resultList.Count)];
     }
 }
