449a315a026e8c5aefcd5a19ce7e9adca6b16b28
2333qbyqby <18317035095@163.com>
2024-04-29 00:48:17 +0800

    修复（疑似）看不到玩家死亡提示bug

--- Assets/Scripts/EnemyAI/EnemyAI.cs
+++ Assets/Scripts/EnemyAI/EnemyAI.cs
@@ -28,7 +28,7 @@ public class EnemyAI : MonoBehaviour
             if (BattleManager.Instance.GetCurrentState() == BattleManager.Instance.GetStates()[GameState.PlayerLose])
             {
                 cancellationToken.Cancel();
-                //return;
+                return;
             }
             try
             {
