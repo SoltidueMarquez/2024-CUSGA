2e7606d1ea60936d2a92c451ca33710999c0b31d
exilecai <1394661877@qq.com>
2024-05-08 17:11:01 +0800

    局内商店so更新

--- Assets/Scripts/BattleStore/BaseResourceEffect.cs
+++ Assets/Scripts/BattleStore/BaseResourceEffect.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 [CreateAssetMenu(fileName = "BaseResourceEffect_", menuName = "BattleStore/BattleProductEffect/BaseResourceEffect")]
 public class BaseResourceEffect : BaseProductEffect
 {
@@ -16,15 +14,20 @@ public class BaseResourceEffect : BaseProductEffect
 
     public override void Use()
     {
+        var player = BattleManager.Instance.parameter.playerChaState.GetComponent<ChaState>();
+        var enemy = BattleManager.Instance.parameter.enemyChaStates[0].GetComponent<ChaState>();
+        DamageInfo damageInfo = new DamageInfo(player.gameObject,enemy.gameObject,
+                                                new Damage(Mathf.Abs(healthChange),0),
+                                                DiceType.Attack,0,null);
         if(target == EffectTarget.Enemy)
         {
-            var enemy = BattleManager.Instance.parameter.enemyChaStates[0].GetComponent<ChaState>();
-            enemy.ModResources(new ChaResource(healthChange, 0, 0, shieldChange,0));
+            
+            enemy.ModResources(new ChaResource(healthChange, 0, 0, shieldChange,0),damageInfo);
         }
         if(target == EffectTarget.Player)
         {
-            var player = BattleManager.Instance.parameter.playerChaState.GetComponent<ChaState>();
-            player.ModResources(new ChaResource(healthChange, 0, 0, shieldChange, 0));
+            
+            player.ModResources(new ChaResource(healthChange, 0, 0, shieldChange, 0),damageInfo);
         }
         
     }
