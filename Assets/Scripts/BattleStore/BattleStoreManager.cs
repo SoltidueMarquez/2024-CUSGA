90528fa8af89fd2a2e559a29b252f82cdd420fb5
exilecai <1394661877@qq.com>
2024-05-09 20:19:36 +0800

    storemanager update

--- Assets/Scripts/BattleStore/BattleStoreManager.cs
+++ Assets/Scripts/BattleStore/BattleStoreManager.cs
@@ -17,19 +17,130 @@ public class BattleStoreManager : MonoSingleton<BattleStoreManager>
     /// 离开商店时调用
     /// </summary>
     public UnityEvent OnExitStore;
+    /// <summary>
+    /// 刷新商品店时调用
+    /// </summary>
+    public UnityEvent OnRefreshStore;
 
-    private void CloseStore()
+    private void Start()
+    {
+        OnEnterStore.AddListener(OpenStore);
+        OnExitStore.AddListener(CloseStore);
+        OnRefreshStore.AddListener(RerollShop);
+    }
+
+    
+
+    public void CloseStore()
     {
 
     }
 
-    private void OpenStore()
+
+    public void OpenStore()
     {
 
     }
 
-    private void RerollShop()
+    public void RerollShop()
+    {
+        battleProducts.Clear();
+        AddProduct();
+    }
+
+    public bool AlreadyHasProduct(BaseBattleProduct product)
     {
 
+        foreach (var item in battleProducts)
+        {
+            if(item.productName == product.productName)
+            {
+               return true;
+            }
+        }
+        return false;
     }
+
+    public void AddProduct()
+    {
+        int i = 0;
+        while (i < 100)
+        {
+            var product = RandomManager.GetBattleProduct(BattleProductType.First);
+            if (!AlreadyHasProduct(product))
+            {
+                battleProducts.Add(product);
+                battleProducts[0].InitialProduct();
+                break;
+            }
+            i++;
+        }
+        i = 0;
+        while (i < 100)
+        {
+            var product = RandomManager.GetBattleProduct(BattleProductType.First);
+            if (!AlreadyHasProduct(product))
+            {
+                battleProducts.Add(product);
+                battleProducts[1].InitialProduct();
+                break;
+            }
+            i++;
+        }
+        i = 0;
+        while (i < 100)
+        {
+            var product = RandomManager.GetBattleProduct(BattleProductType.Second);
+            if (!AlreadyHasProduct(product))
+            {
+                battleProducts.Add(product);
+                battleProducts[2].InitialProduct();
+                break;
+            }
+
+            i++;
+        }
+        i = 0;
+        while (i < 100)
+        {
+            var product = RandomManager.GetBattleProduct(BattleProductType.Second);
+            if (!AlreadyHasProduct(product))
+            {
+                battleProducts.Add(product);
+                battleProducts[3].InitialProduct();
+                break;
+            }
+
+            i++;
+        }
+        i = 0;
+        while (i < 100)
+        {
+            var product = RandomManager.GetBattleProduct(BattleProductType.Third);
+            if (!AlreadyHasProduct(product))
+            {
+                battleProducts.Add(product);
+                battleProducts[4].InitialProduct();
+                break;
+            }
+
+            i++;
+        }
+        i = 0;
+        while (i < 100)
+        {
+            var product = RandomManager.GetBattleProduct(BattleProductType.Third);
+            if (!AlreadyHasProduct(product))
+            {
+                battleProducts.Add(product);
+                battleProducts[5].InitialProduct();
+                break;
+            }
+
+            i++;
+        }
+
+
+    }
+    
 }
