74b7dcea77679916d4e1a9ea61e9081441027bb5
SoltidueMarquez <2630388173@qq.com>
2024-05-10 16:48:46 +0800

    局内商店UI搭建中

---
+++ Assets/Scripts/UI/SideStoreUIManager.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class SideStoreUIManager : MonoSingleton<SideStoreUIManager>
+    {
+        [SerializeField, Tooltip("骰子栏位列表")] public List<Column> productColumns;
+        [SerializeField, Tooltip("货币文本")] public List<Text> costTextList;
+        [SerializeField, Tooltip("骰子模板")] private GameObject productTemplate;
+        [SerializeField, Tooltip("更新进度条")] private Slider updateSlider;
+        [SerializeField, Tooltip("动画时长")] private float animTime;
+        [SerializeField, Tooltip("预览大小")] private float previewSize;
+        [Header("开启关闭相关")]
+        [SerializeField, Tooltip("商店UI界面")] private GameObject sideStoreUI;
+        [SerializeField, Tooltip("隐藏位置")] private Transform hidePosition;
+        [SerializeField, Tooltip("出现位置")] private Transform showPosition;
+        [SerializeField, Tooltip("遮罩")] private GameObject mask;
+        
+        //创建商品
+        private void CreateProductUI(int index, Action onBuy)
+        {
+            if (index > productColumns.Count)
+            {
+                Debug.LogWarning("超出生成的栏位");
+                return;
+            }
+            if (productColumns[index].bagObject != null) 
+            {
+                Debug.LogWarning("栏位已经有商品");
+                return;
+            }
+            var parent = productColumns[index].transform;
+            var tmp = Instantiate(productTemplate, parent, true);
+            productColumns[index].bagObject = tmp;
+            tmp.transform.position = parent.position;//更改位置
+            
+            var tmpProduct = tmp.GetComponent<SideStoreProductUIObject>();
+            tmpProduct.Init(animTime, previewSize, onBuy);//初始化
+            
+            //TODO:售价文本初始化
+            costTextList[index].text = "???";//售价文本初始化
+            
+            tmp.SetActive(true);
+            tmpProduct.DoAppearAnim(animTime); //出现动画
+        }
+        
+        //移除商品
+        private void RemoveProductUI(int index)
+        {
+            if (index > productColumns.Count)
+            {
+                Debug.LogWarning("超出生成的栏位");
+                return;
+            }
+            if (productColumns[index].bagObject == null) 
+            {
+                return;
+            }
+            var item = productColumns[index].bagObject.GetComponent<SideStoreProductUIObject>();
+            productColumns[index].bagObject = null;
+            if (item != null)
+            {
+                item.DoDestroyAnim(animTime, 0);
+            }
+        }
+        
+        //刷新全部商品
+        public void RefreshProductUI()
+        {
+            for (int i = 0; i < productColumns.Count; i++)
+            {
+                RemoveProductUI(i);
+            }
+
+            for (int i = 0; i < productColumns.Count; i++)
+            {
+                CreateProductUI(i, null);
+            }
+        }
+
+        //打开商店
+        public void OpenStore()
+        {
+            sideStoreUI.transform.DOMove(showPosition.position, animTime).OnComplete(() =>
+            {
+                mask.SetActive(false);
+            });
+        }
+        
+        //关闭商店
+        public void CloseStore()
+        {
+            mask.SetActive(true);
+            sideStoreUI.transform.DOMove(hidePosition.position, animTime);
+        }
+        
+        //更新进度条
+        public void UpdateSliderValue(float value, float maxValue)
+        {
+            updateSlider.value = value / maxValue;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                RefreshProductUI();
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                OpenStore();
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                CloseStore();
+            }
+        }
+    }
+}
